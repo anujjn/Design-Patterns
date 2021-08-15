20a8ad18d40ccd103c7633c5587522068b5d9ef7
Anuj Jain <anujjain2882@gmail.com>
2021-08-15 18:01:09 +0530

    Thread safety in singleton

--- src/Design Patterns/Singleton Design Pattern/Program.cs
+++ src/Design Patterns/Singleton Design Pattern/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Singleton_Design_Pattern
 {
@@ -6,10 +7,11 @@ namespace Singleton_Design_Pattern
     {
         static void Main(string[] args)
         {
-            // assuming singleton class is invoked from fromEmployee class
+            // assuming singleton class is invoked from Employee class
             Singleton fromEmployee = Singleton.GetInstance;
             fromEmployee.PrintDetails("Message from employee");
 
+            // assuming singleton class is invoked from Student class
             Singleton fromStudent = Singleton.GetInstance;
             fromStudent.PrintDetails("Messaage from student");
 
@@ -19,7 +21,28 @@ namespace Singleton_Design_Pattern
             //Singleton.DerivedSingleton derivedObj = new Singleton.DerivedSingleton();
             //derivedObj.PrintDetails("From Derived");
 
+            /*
+             * Singleton Thread Safety Scenario
+             * This will create multiple instance of SingletonThreadSafety
+             * if thread safe SingletonThreadSafety is not created - use lock
+             */
+            Parallel.Invoke(PrintEmployeeDetails, PrintStudentDetails);
+
             Console.ReadLine();
+
+
+        }
+
+        private static void PrintEmployeeDetails()
+        {
+            SingletonThreadSafety fromEmployee = SingletonThreadSafety.GetInstance;
+            fromEmployee.PrintMessage("From Employee");
+        }
+
+        private static void PrintStudentDetails()
+        {
+            SingletonThreadSafety fromEmployee = SingletonThreadSafety.GetInstance;
+            fromEmployee.PrintMessage("From Student");
         }
     }
 }
---
+++ src/Design Patterns/Singleton Design Pattern/SingletonThreadSafety.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton_Design_Pattern
+{
+    public sealed class SingletonThreadSafety
+    {
+        private static int counter = 0;
+        private static SingletonThreadSafety instance = null;
+
+        /*
+        * Lazy Initialization in Singleton : GetInstance Property is responsible for the 
+        * SingletonThreadSafety Instance creation. SingletonThreadSafety object is not created
+        * until and unless GetInstance is invoked. Hence, there is a delay in instance 
+        * creation till the GetInstance is accessed.  This Delay in Instance creation is 
+        * called Lazy Initialization.
+        */
+
+        /*
+         * There is a chance that we may end up creating multiple instances 
+         * when multiple threads (Parallel.Invoke ) invoke the GetInstance at the same time.
+         
+         * This Thread racing situation causes thread safety issues in Singleton Initialization and 
+         * further the current code ends up in creating multiple instances of Singleton objects in 
+         * memory. 
+         
+         * Locks are the best way to control thread race condition
+         */
+
+        private static readonly  object lockObject = new object();
+        public static SingletonThreadSafety GetInstance 
+        {
+            get 
+            {
+                // Check lock only if instance is null - Double checked locking
+                if (instance == null)
+                {
+                    // For thread safety
+                    lock (lockObject)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new SingletonThreadSafety();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+        private SingletonThreadSafety()
+        {
+            counter++;
+            Console.WriteLine("Counter Value" + counter.ToString());
+        }
+
+        public void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
