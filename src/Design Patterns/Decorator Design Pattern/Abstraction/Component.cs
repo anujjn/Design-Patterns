c4206258bb97e7242055a451586e41915682adc6
Anuj Jain <anujjain2882@gmail.com>
2022-07-01 02:00:39 +0530

    Decorator Design Pattern

---
+++ src/Design Patterns/Decorator Design Pattern/Abstraction/Component.cs
@@ -0,0 +1,9 @@
+﻿namespace Decorator_Design_Pattern
+{
+    // The base Component interface defines operations that can be altered by
+    // decorators.
+    public abstract class Component
+    {
+        public abstract string Operation();
+    }
+}
